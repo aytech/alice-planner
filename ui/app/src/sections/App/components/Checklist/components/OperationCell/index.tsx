5fd540e43ed9973f55c5d6449a8c1ffbf789e3a4
Oleg Yapparov <oyapparov@gmail.com>
2022-07-14 15:03:38 +0200

    Fix editable buttons transformations

--- ui/app/src/sections/App/components/Checklist/components/OperationCell/index.tsx
+++ ui/app/src/sections/App/components/Checklist/components/OperationCell/index.tsx
@@ -1,5 +1,5 @@
 import { CloseCircleOutlined, EditOutlined, SaveOutlined, SnippetsOutlined } from "@ant-design/icons";
-import { Button, Space, Tooltip, Typography } from "antd";
+import { Button, Space, Tooltip } from "antd";
 import { useTranslation } from "react-i18next";
 import { IChecklistTableItem } from "../../../../../../lib/Types"
 
@@ -34,40 +34,69 @@ export const OperationCell = ({
     )
   }
 
-  return editing ? (
-    <Space>
-      <Tooltip title={ t("save") } placement="top">
-        <Button
-          icon={ <SaveOutlined /> }
-          onClick={ () => save(record) }
-          size="small"
-          style={ { marginRight: 6 } } />
-      </Tooltip>
-      <Tooltip title={ t("cancel") } placement="top">
-        <Button
-          icon={ <CloseCircleOutlined /> }
-          onClick={ cancel }
-          size="small" />
-      </Tooltip>
-    </Space>
-  ) : (
-    <Space>
-      <Tooltip title={ t("edit") } placement="top">
-        <Button
-          disabled={ editingKey !== '0' }
-          icon={ <EditOutlined /> }
-          onClick={ () => edit(record) }
-          size="small"
-          style={ { marginRight: 6 } } />
-      </Tooltip>
-      <Tooltip title={ t("archive") } placement="top">
-        <Button
-          disabled={ editingKey !== '0' }
-          icon={ <SnippetsOutlined /> }
-          onClick={ () => console.log("archiving") }
-          size="small"
-          style={ { marginRight: 8 } } />
-      </Tooltip>
-    </Space>
-  );
+  const EditingActions = () => {
+    return record.id === '0' ? (
+      <Space>
+        <Tooltip title={ t("save") } placement="top">
+          <Button
+            icon={ <SaveOutlined /> }
+            onClick={ () => save(record) }
+            size="small"
+            style={ { marginRight: 6 } } />
+        </Tooltip>
+      </Space>
+    ) : (
+      <Space>
+        <Tooltip title={ t("save") } placement="top">
+          <Button
+            icon={ <SaveOutlined /> }
+            onClick={ () => save(record) }
+            size="small"
+            style={ { marginRight: 6 } } />
+        </Tooltip>
+        <Tooltip title={ t("cancel") } placement="top">
+          <Button
+            icon={ <CloseCircleOutlined /> }
+            onClick={ cancel }
+            size="small" />
+        </Tooltip>
+      </Space>
+    )
+  }
+
+  const ReadingActions = () => {
+    return record.id === '0' ? (
+      <Space>
+        <Tooltip title={ t("save") } placement="top">
+          <Button
+            disabled={ editingKey !== '0' }
+            icon={ <SaveOutlined /> }
+            onClick={ () => save(record) }
+            size="small"
+            style={ { marginRight: 6 } } />
+        </Tooltip>
+      </Space>
+    ) : (
+      <Space>
+        <Tooltip title={ t("edit") } placement="top">
+          <Button
+            disabled={ editingKey !== '0' }
+            icon={ <EditOutlined /> }
+            onClick={ () => edit(record) }
+            size="small"
+            style={ { marginRight: 6 } } />
+        </Tooltip>
+        <Tooltip title={ t("archive") } placement="top">
+          <Button
+            disabled={ editingKey !== '0' }
+            icon={ <SnippetsOutlined /> }
+            onClick={ () => console.log("archiving") }
+            size="small"
+            style={ { marginRight: 8 } } />
+        </Tooltip>
+      </Space>
+    )
+  }
+
+  return editing ? <EditingActions /> : <ReadingActions />
 }
